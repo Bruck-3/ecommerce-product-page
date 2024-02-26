0fc16d3719bf05ba42ab7979950141bda7f6021a
Bruck-3 <brucktafesse@yahoo.com>
2024-02-26 14:56:31 +0300

    navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,25 +1,50 @@
 <!-- src/views/Home.vue -->
 <script setup>
+import { ref } from "vue";
 const navigationItems = ["Collections", "Men", "Women", "About", "Contact"];
+const isMobileNavOpen = ref(false);
+
+const toggleMobileNav = () => {
+  isMobileNavOpen.value = !isMobileNavOpen.value;
+};
+
+const closeMobileNav = () => {
+  isMobileNavOpen.value = false;
+};
+
 </script>
 <template>
-  <nav class="bg-white border-gray-200 md:px-[200px]">
-    <div class="max-w-screen-xl flex justify-between mx-auto p-4">
-      <div class="flex gap-x-[90px] items-center">
+  <nav class="bg-white border-gray-200 lg:px-[100px] xl:px-[200px]">
+    <div
+      class="max-w-screen-xl flex items-center justify-between mx-auto p-4 h-[70px]"
+    >
+      <div class="flex justify-between items-center">
+        <img
+          src="../assets/icon-menu.svg"
+          class="mr-[20px] pt-[4px] flex lg:hidden"
+          alt=""
+          @click="toggleMobileNav"
+        />
         <img src="../assets/logo.svg" class="w-[138px] h-[20px]" alt="Logo" />
-        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul class="font-medium flex gap-x-[40px] items-center">
+        <div class="hidden w-full lg:block md:w-auto" id="navbar-default">
+          <ul
+            class="font-medium flex justify-between lg:w-[350px] xl:w-[400px] items-center"
+          >
             <li
               class="list-none text-gray-500 cursor-pointer"
               v-for="(nav, index) in navigationItems"
               :key="index"
             >
-              <p class>{{ nav }}</p>
+              <p
+                class="hover:border-0 hover:border-spacing-2 hover:border-b-[5px] hover:border-primaryOrange hover:border-solid transitio py-[72px]"
+              >
+                {{ nav }}
+              </p>
             </li>
           </ul>
         </div>
       </div>
-      <div class="flex gap-x-[30px] md:gap-x-[60px] items-center">
+      <div class="flex w-[100px] lg:w-[100px] items-center justify-between">
         <img
           src="../assets/icon-cart.svg"
           class="h-[20px] w-[20px] cursor-pointer"
@@ -27,13 +52,39 @@ const navigationItems = ["Collections", "Men", "Women", "About", "Contact"];
         />
         <img
           src="../assets/image-avatar.png"
-          class="h-[40px] w-[40px]"
+          class="h-[40px] w-[40px] rounded-[50%] hover:border-solid hover:border-[2px] hover:border-primaryOrange cursor-pointer"
           alt=""
         />
       </div>
     </div>
-    <div class=" w-full">
-        <span class="border-t border-gray-300"></span>
+    <hr class="h-px my-8 bg-gray-200 border-0 hidden lg:flex" />
+    <div v-show="isMobileNavOpen" class="fixed top-0 left-0 z-30 w-full h-full bg-black bg-opacity-50" @click="closeMobileNav"></div>
+    <div
+      v-show="isMobileNavOpen"
+      id="drawer-example"
+      class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-gray-50 w-[40%] py-[50px] px-[30px] rounded-r"
+      aria-labelledby="drawer-label"
+    >
+      <!-- Close Navigation Icon -->
+      <div class="flex justify-start mb-[60px]">
+        <img
+          src="../assets/icon-close.svg"
+          alt=""
+          @click="toggleMobileNav"
+          class
+        />
+      </div>
+
+      <!-- Navigation Items for Mobile -->
+        <div
+          class="list-none text-black font-semibold cursor-pointer"
+          v-for="(nav, index) in navigationItems"
+          :key="index"
+        >
+          <p class="my-[25px]">
+            {{ nav }}
+          </p>
+        </div>
     </div>
   </nav>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router';
+import VueEasyLightbox from 'vue-easy-lightbox'
 
-createApp(App).use(router).mount('#app');
+
+createApp(App).use(router).use(VueEasyLightbox).mount('#app');
